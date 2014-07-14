f75292302643dc5406994c95d9867dc4edea3314
Phil <peva0411@gmail.com>
2014-07-14 07:26:06 -0400

    init commit

---
+++ MessageQueue.Messaging/Impl/MessageQueueBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageQueue.Messaging.Spec;
+
+namespace MessageQueue.Messaging.Impl
+{
+    public abstract class MessageQueueBase:IDisposable
+    {
+        public Direction Direction { get; set; }
+        public MessagePattern Pattern { get; set; }
+        public string Address { get; private set; }
+        public Dictionary<string, object> Properties { get; set; }
+
+        protected virtual void Initialize(Direction direction, string name, MessagePattern pattern, Dictionary<string, object> properties)
+        {
+            Direction = direction;
+            Pattern = pattern;
+            Address = GetAddress(name);
+            Properties = properties ?? new Dictionary<string, object>();
+        }
+
+        protected void RequireProperty<T>(string name)
+        {
+            
+        }
+
+        protected abstract void InitializeOutbound(string name, MessagePattern pattern, Dictionary<string, object> properties);
+
+        protected abstract void IntializeInbound(string name, MessagePattern pattern, Dictionary<string, object> properties);
+
+        protected abstract void Send(Message message);
+
+        protected abstract void Listen(Action<Message> onMessageReceived);
+
+        protected abstract void Receive(Action<Message> onMessageReceived);
+
+        protected abstract string GetAddress(string name);
+
+        protected abstract IMessageQueue GetResponseQueue();
+
+        protected abstract IMessageQueue GetReplyQueue(Message message);
+
+        public abstract void Dispose();
+    }
+}
---
+++ MessageQueue.Messaging/Impl/Msmq/MsmqMessageQueue.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MessageQueue.Messaging.Spec;
+using msmq = System.Messaging;
+
+namespace MessageQueue.Messaging.Impl.Msmq
+{
+    public class MsmqMessageQueue : MessageQueueBase
+    {
+        private msmq.MessageQueue _queue;
+        private bool _useTemporaryQueue;
+
+
+        protected override void InitializeOutbound(string name, MessagePattern pattern, Dictionary<string, object> properties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void IntializeInbound(string name, MessagePattern pattern, Dictionary<string, object> properties = null)
+        {
+            Initialize(Direction.Inbound, name, pattern, properties);
+            switch (pattern)
+            {
+                case MessagePattern.PublishSubscribe:
+                    _queue = new msmq.MessageQueue(Address);
+                    break;
+
+                case MessagePattern.RequestResponse:
+                    _queue = _useTemporaryQueue ? msmq.MessageQueue.Create(Address) : new msmq.MessageQueue(Address);
+                    break;
+                default:
+                    _queue = new msmq.MessageQueue(Address);
+                    break;
+            }
+        }
+
+        protected override void Send(Message message)
+        {
+            var outbound = new msmq.Message();
+            outbound.BodyStream = message.ToJsonStream();
+            if (!string.IsNullOrEmpty(message.ResponseAddress))
+            {
+                outbound.ResponseQueue = new msmq.MessageQueue(message.ResponseAddress);
+            }
+            _queue.Send(outbound);
+        }
+
+        protected override void Listen(Action<Message> onMessageReceived)
+        {
+            while (true)
+            {
+                Receive(onMessageReceived);
+            }
+        }
+
+        protected override void Receive(Action<Message> onMessageReceived)
+        {
+            var inbound = _queue.Recieve();
+            var message = Message.FromJson(inbound.BodyStream);
+            onMessageReceived(message);
+        }
+
+        protected override string GetAddress(string name)
+        {
+            if (Pattern == MessagePattern.RequestResponse && Direction == Direction.Inbound)
+            {
+                _useTemporaryQueue = true;
+                return string.Format(".\\private$\\{0}", Guid.NewGuid().ToString().Substring(0, 6));
+            }
+
+            switch (name.ToLower())
+            {
+                case "unsubscribe":
+                    return ".\\private$\\messagequeue.unsubscribe";
+                case "doesuserexist":
+                    return ".\\private$\\messagequeue.doesuserexist";
+                case "unsubscribe-legacy":
+                    return ".\\private$\\messagequeue.unsubscribe-legacy";
+                case "unsubscribed-event":
+                    return "FormatName:MULTICAST=234.1.1.2:8001";
+                case "unsubscribe-crm":
+                    return ".\\private$\\messagequeue.unsubscribe-crm";
+                case "unsubscribe-fulfilment":
+                    return ".\\private$\\messagequeue.unsubscribe-fulfilment";
+            }
+
+        }
+
+        protected override IMessageQueue GetResponseQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IMessageQueue GetReplyQueue(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
